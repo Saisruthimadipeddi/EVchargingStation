4fdfe07ad92171c4a1075a0bfbbe27ee5416c3d1
johan12345 <johan.forstner@gmail.com>
2023-05-06 22:57:05 +0200

    cleanup saved regions in CleanupCacheWorker

--- app/src/main/java/net/vonforst/evmap/storage/CleanupCacheWorker.kt
+++ app/src/main/java/net/vonforst/evmap/storage/CleanupCacheWorker.kt
@@ -4,14 +4,23 @@ import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import net.vonforst.evmap.api.createApi
+import java.time.Instant
 
 class CleanupCacheWorker(appContext: Context, workerParams: WorkerParameters) :
     CoroutineWorker(appContext, workerParams) {
     override suspend fun doWork(): Result {
-        val dao = AppDatabase.getInstance(applicationContext).chargeLocationsDao()
-        for (type in listOf("openchargemap", "goingelectric")) {
-            val api = createApi(type, applicationContext)
-            dao.deleteOutdatedIfNotFavorite(type, api.cacheLimitDate())
+        val db = AppDatabase.getInstance(applicationContext)
+
+        val chargeLocations = db.chargeLocationsDao()
+        val savedRegionDao = db.savedRegionDao()
+        val now = Instant.now()
+
+        val dataSources = listOf("openchargemap", "goingelectric")
+        for (dataSource in dataSources) {
+            val api = createApi(dataSource, applicationContext)
+            val limit = now.minus(api.cacheLimit).toEpochMilli()
+            chargeLocations.deleteOutdatedIfNotFavorite(dataSource, limit)
+            savedRegionDao.deleteOutdated(dataSource, limit)
         }
         return Result.success()
     }
